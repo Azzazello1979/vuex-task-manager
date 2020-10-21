502b1d7d95a9b1fcad453be7cfcb7b15ba81203b
Balint H <balint.haui@gmail.com>
2020-10-21 11:11:37 +0200

    fetching iceandfire characters

--- src/store/modules/todosModule.js
+++ src/store/modules/todosModule.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-const state = {};
-const getters = {};
-const actions = {};
+const state = {
+  todos: [
+    { id: 1, title: "Todo1" },
+    { id: 2, title: "Todo2" },
+    { id: 3, title: "Todo3" },
+  ],
+};
+const getters = {
+  allTodos: (state) => state.todos,
+};
+const actions = {
+  async fetchCharacters() {
+    const response = await axios.get(
+      "https://www.anapioficeandfire.com/api/characters"
+    );
+    console.log(response.data);
+  },
+};
 const mutations = {};
 
 export default {
